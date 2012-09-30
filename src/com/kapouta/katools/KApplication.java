f59766cbe7d67583d1ae10e69f39c11e268ed5dd
feiteira <feiteira@gmail.com>
2012-09-30 22:13:45 +0200

    v2

--- src/com/kapouta/katools/KApplication.java
+++ src/com/kapouta/katools/KApplication.java
@@ -3,5 +3,14 @@ package com.kapouta.katools;
 import android.app.Application;
 
 public abstract class KApplication extends Application {
+	protected static KApplication self;
 
+	public void onCreate() {
+		super.onCreate();
+		self = this;
+		
+	}
+
+	
+	
 }
