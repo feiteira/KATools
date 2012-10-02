9551ca2833c16f0c69bb33c6168bdd0539fd86a8
José Feiteirinha <feiteira@gmail.com>
2012-10-02 09:25:21 +0200

    Latest version.

--- src/com/kapouta/katools/KApplication.java
+++ src/com/kapouta/katools/KApplication.java
@@ -1,16 +1,17 @@
 package com.kapouta.katools;
 
+
 import android.app.Application;
 
 public abstract class KApplication extends Application {
 	protected static KApplication self;
 
+	public abstract String getAnalyticsUACode();
+	
 	public void onCreate() {
 		super.onCreate();
 		self = this;
-		
 	}
 
-	
 	
 }
