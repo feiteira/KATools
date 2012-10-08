db2c3872f16e39d3a66686090ddab0abf9457576
José Feiteirinha <feiteira@gmail.com>
2012-10-08 18:34:05 +0200

    task 3:
    Added extends to KRenderer but not yet populated.

---
+++ src/com/kapouta/katools/KRenderer.java
@@ -0,0 +1,25 @@
+package com.kapouta.katools;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class KRenderer implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 arg0) {
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 arg0, int arg1, int arg2) {
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+		
+	}
+
+}
