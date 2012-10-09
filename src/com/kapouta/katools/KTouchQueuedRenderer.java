c3d71fb598333485f5f9fcf176d92a8636948f24
José Feiteirinha <feiteira@gmail.com>
2012-10-09 13:51:48 +0200

    IN PROGRESS - task 3: Mouse Events into Queue and handled by the GUI http://spreadsheets.google.com/ccc?key=0AtJ_N9HxeIfOdHptMG9uRkhDaWYzS3l3cDBKTWNDT3c
    Added some methods to the KTouchQueuedRenderer
    onTouchDown, onDualTouchDown, onTouchMove, ...

--- src/com/kapouta/katools/KTouchQueuedRenderer.java
+++ src/com/kapouta/katools/KTouchQueuedRenderer.java
@@ -22,10 +22,35 @@ public abstract class KTouchQueuedRenderer implements Renderer, OnTouchListener 
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		if (events.size() > 0)
-			this.onDrawFrame(gl, events.remove(0));
-		else
-			this.onDrawFrame(gl, null);
+		if (events.size() > 0) {
+			MotionEvent curr_event = events.remove(0);
+
+			switch (curr_event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				this.onTouchDown(curr_event.getX(), curr_event.getY());
+				break;
+			case MotionEvent.ACTION_MOVE:
+				this.onTouchMove(curr_event.getX(), curr_event.getY());
+				break;
+			case MotionEvent.ACTION_UP:
+				this.onTouchUp(curr_event.getX(), curr_event.getY());
+				break;
+			case MotionEvent.ACTION_POINTER_2_DOWN:
+				int pid0 = curr_event.getPointerId(0);
+				int pid1 = curr_event.getPointerId(1);
+				this.onTouchUp(curr_event.getX(pid0), curr_event.getY(pid0));
+				this.onDualTouchDown(curr_event.getX(pid0),
+						curr_event.getY(pid0), curr_event.getX(pid1),
+						curr_event.getY(pid1));
+				break;
+			case MotionEvent.ACTION_POINTER_2_UP:
+				break;
+				default:
+				
+			}
+
+		}
+		this.onDrawFrame(gl, null);
 	}
 
 	@Override
@@ -36,6 +61,24 @@ public abstract class KTouchQueuedRenderer implements Renderer, OnTouchListener 
 	@Override
 	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
 
+	}
+
+	public void onTouchDown(float x, float y) {
+	}
+
+	public void onTouchMove(float x, float y) {
+	}
+
+	public void onTouchUp(float x, float y) {
+	}
+
+	public void onDualTouchDown(float x1, float y1, float x2, float y2) {
+	}
+
+	public void onDualTouchMove(float x1, float y1, float x2, float y2) {
+	}
+
+	public void onDualTouchUp(float x1, float y1, float x2, float y2) {
 	}
 
 	@Override
