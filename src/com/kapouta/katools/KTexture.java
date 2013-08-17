fdf271fca63184a387d5c10a2c73e31a5906e50f
Jose Luis Feiteirinha <feiteira@gmail.com>
2013-08-17 14:46:41 +0200

    Minor fixes on KTexture.

--- src/com/kapouta/katools/KTexture.java
+++ src/com/kapouta/katools/KTexture.java
@@ -7,7 +7,7 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 public class KTexture {
-	static final float[] SQUARE_TEX_VERTEXES = {
+	static final float[] DEFAULT_SQUARE_TEX_VERTEXES = {
 			// cenas
 			-1.0f, -1.0f, 0.0f, // V1 - bottom left
 			-1.0f, 1.0f, 0.0f, // V2 - top left
@@ -15,7 +15,7 @@ public class KTexture {
 			1.0f, 1.0f, 0.0f // V4 - top right
 	};
 
-	static final float TEX_COORDINATES[] = {
+	static final float DEFAULT_TEX_COORDINATES[] = {
 			// Mapping coordinates for the vertices
 			0.0f, 1.0f, // top left (V2)
 			0.0f, 0.0f, // bottom left (V1)
@@ -29,17 +29,49 @@ public class KTexture {
 	public FloatBuffer texture; // buffer holding the texture
 								// coordinates
 
+	public static KTexture createTexture(String filepath, float z){
+		return new KTexture(filepath, z);
+	}
+
+	public static KTexture createTexture(String filepath, float glx1,
+			float gly1, float glx2, float gly2, float z) {
+		KTexture tex = new KTexture();
+
+		ByteBuffer byteBuffer = ByteBuffer
+				.allocateDirect(DEFAULT_TEX_COORDINATES.length * 4);
+		byteBuffer.order(ByteOrder.nativeOrder());
+		tex.texture = byteBuffer.asFloatBuffer();
+		tex.texture.put(DEFAULT_TEX_COORDINATES);
+		tex.texture.position(0);
+
+		tex.vertexes = new float[] {
+				// cenas
+				glx1, gly1, z, // V1 - bottom left
+				glx1, gly2, z, // V2 - top left
+				glx2, gly1, z, // V3 - bottom right
+				glx2, gly2, z // V4 - top right
+		};
+
+		tex.makeBuffer();
+
+		return tex;
+	}
+
+	private KTexture() {
+
+	}
+
 	public KTexture(String filepath, float z) {
 		// / KTexture texData = new KTexture();
 
 		ByteBuffer byteBuffer = ByteBuffer
-				.allocateDirect(TEX_COORDINATES.length * 4);
+				.allocateDirect(DEFAULT_TEX_COORDINATES.length * 4);
 		byteBuffer.order(ByteOrder.nativeOrder());
 		this.texture = byteBuffer.asFloatBuffer();
-		this.texture.put(TEX_COORDINATES);
+		this.texture.put(DEFAULT_TEX_COORDINATES);
 		this.texture.position(0);
 
-		this.vertexes = SQUARE_TEX_VERTEXES;
+		this.vertexes = DEFAULT_SQUARE_TEX_VERTEXES;
 		this.setDepth(z);
 		this.makeBuffer();
 
@@ -54,15 +86,15 @@ public class KTexture {
 	public void makeBuffer() {
 		ByteBuffer vbb2 = ByteBuffer.allocateDirect(
 		// (# of coordinate values * 4 bytes per float)
-				SQUARE_TEX_VERTEXES.length * 4);
+				DEFAULT_SQUARE_TEX_VERTEXES.length * 4);
 		vbb2.order(ByteOrder.nativeOrder());// use the device hardware's
 											// native
 											// byte order
 		buffer = vbb2.asFloatBuffer(); // create a floating point buffer
 										// from
 										// the ByteBuffer
-		buffer.put(SQUARE_TEX_VERTEXES); // add the coordinates to the
-											// FloatBuffer
+		buffer.put(DEFAULT_SQUARE_TEX_VERTEXES); // add the coordinates to the
+		// FloatBuffer
 
 		buffer.position(0); // set the buffer to read the first
 							// coordinate
